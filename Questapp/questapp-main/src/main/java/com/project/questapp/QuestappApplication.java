aa05822c8a2941517fb0c97684e1cec25311d0a6
Nurcan Yaz <97188298+nurcanyaz@users.noreply.github.com>
2024-07-17 10:38:21 +0300

    Add files via upload

---
+++ Questapp/questapp-main/src/main/java/com/project/questapp/QuestappApplication.java
@@ -0,0 +1,13 @@
+package com.project.questapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  ConnappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestappApplication.class, args);
+	}
+
+}
